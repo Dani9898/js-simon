6a7b50d1cdc8e0b4e7f885b2a23fc9565a6c00de
Dani9898 <danieleoliva98@gmail.com>
2021-11-09 16:31:42 +0100

    es finito

--- js/script.js
+++ js/script.js
@@ -1,10 +1,66 @@
 // Visualizzare in pagina 5 numeri casuali => array=[]; while (array.length < 5) {numero = mathfloor mathrandom; while (array.includes(numero); numero = mathfloor mathrandom)
 // Da lì parte un timer di 30 secondi => setTimeout tempo = 30 tempo-- innerHTML tempo
-// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() => if tempo === 0  numero1= parseInt(prompt()) 
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() => if tempo === 0  while array.length < 5 numero = parseInt(prompt()), push numero
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+    // confrontare il primo numero dell'array dell'utente con ogni numero dell'array casuale. Se c'è corrispondenza allora salvare quello nell'array dei numeri indovinati. Confrontare poi gli altri. => for (let i = 0; i < array.length; i++) { if (numeriCasuali.includes(numeriUtente[i]) { nuovoarray.push[numeriUtente[i]]}}
+
 
     const numeriCasuali = document.getElementById("numeri");
     const countdown = document.getElementById("timer");
-    const risposta = document.getElementById("risultato");
 
-    
+    const randomNumbers = [];
+    while (randomNumbers.length < 5) {
+        let numero = Math.floor(Math.random() * 100 + 1);
+        while (randomNumbers.includes(numero)) {
+            numero = Math.floor(Math.random() * 100 + 1);
+        }
+        randomNumbers.push(numero)
+    }
+console.log(randomNumbers);
+
+randomNumbers.forEach((elemento) => {
+    numeriCasuali.innerHTML += `${elemento} `;
+})
+
+let tempo = 5;
+countdown.innerHTML = tempo;
+
+const numeriUtente = [];
+const numeriIndividuati = [];
+
+const clock = setInterval(myFunction, 1000);
+function myFunction() {
+    tempo--;
+    countdown.innerHTML = tempo;
+    if (tempo === 0) {
+        countdown.innerHTML = "";
+
+        clearInterval(clock);
+
+        while (numeriUtente.length < 5) {
+            let numeroChiesto = parseInt(prompt("Inserisci il numero"));
+
+            while (isNaN(numeroChiesto)) {
+            numeroChiesto = parseInt(prompt("Inserisci il numero"));
+            }
+            numeriUtente.push(numeroChiesto)
+        }
+        
+        console.log(numeriUtente);
+
+        for (let i = 0; i < randomNumbers.length; i++) {
+            if (randomNumbers.includes(numeriUtente[i])) {
+                numeriIndividuati.push(numeriUtente[i])   
+            }
+        }
+
+        console.log(numeriIndividuati);
+        numeriCasuali.innerHTML = "";
+
+        numeriIndividuati.forEach((elemento) => {
+            numeriCasuali.innerHTML += `${elemento} `
+        })
+
+    }
+}
+
